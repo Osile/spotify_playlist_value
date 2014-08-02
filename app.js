22dadd2ded63191c0c533f4d405e17b8a95bc497
Osile <mo@onlinestage4you.de>
2014-08-03 00:42:37 +0200

    added spotify auth

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 $('#btnAuth').click(function(el){
-       $.get('https://accounts.spotify.com/authorize',{
-         client_id: 'd7b34968b76040198e003e715c4da88a',
-         response_type: 'code',
-         redirect_uri: 'osile.github.com/spotify_playlist_value'
-       },function(res){
-         alert('yeah');
-       });
+       var url = 'https://accounts.spotify.com/authorize/';
+       var client_id = 'd7b34968b76040198e003e715c4da88a';
+       var response_type = 'code';
+       var redirect_uri = encodeURIComponent('http://osile.github.io/spotify_playlist_value');
+
+       window.location.href = url + '?client_id=' + client_id + '&response_type=' + response_type + '&redirect_uri=' + redirect_uri;
 });
