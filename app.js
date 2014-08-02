9fb68c99e973e2c87b7ffd6a503b06b90cc43bb9
Osile <mo@onlinestage4you.de>
2014-08-03 00:05:17 +0200

    changed get methode

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 $('#btnAuth').click(function(el){
-       $.ajax({
-            url: "https://accounts.spotify.com/authorize",
-            type: "GET",
-            data: {
-              client_id: 'd7b34968b76040198e003e715c4da88a',
-              response_type: 'code',
-              redirect_uri:''
-            },
-            async: false,
-            success:function(msg){
-
-            }
-        });
+       $.get('https://accounts.spotify.com/authorize',{
+         client_id: 'd7b34968b76040198e003e715c4da88a',
+         response_type: 'code',
+         redirect_uri: 'osile.github.com/spotify_playlist_value'
+       },function(res){
+         alert('yeah');
+       });
 });
